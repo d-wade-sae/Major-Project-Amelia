e9fc94e007ad36248bfdcc1f56a3e24c8149986c
d-wade-sae <damon1w@optusnet.com.au>
2017-11-20 20:26:53 +1100

    Starting work on detecting

---
+++ Major Project Amelia/Assets/Scripts/DetectorTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorTile : MonoBehaviour {
+
+    // Public Variables
+    [Header("Tile Variables")]
+    public string tileName; // name of tile
+    public string region; // region for tile (enemies that spawn)
+    public int tilePercentage; // 
+
+    // Private Variables
+    private GameObject player;
+    private GameManager GM; // Game Manager
+    private int randomNumber;
+
+
+	void Start ()
+    {
+        GM = GameObject.Find("_GameManager").GetComponent<GameManager>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
---
+++ Major Project Amelia/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("Player Variables")]
+    public GameObject player;
+
+    [Header("Cameras")]
+    public GameObject worldCamera;
+
+    [Header("Canvas")]
+    public bool enableDebug;
+    public GameObject debugCanvas;
+
+    [Header("World Variables")]
+    public GameObject world;
+
+
+    void Awake()
+    {
+        // Enabling Debug Canvas
+        if (enableDebug == true)
+        {
+            debugCanvas.SetActive(true);
+        }
+
+        else
+        {
+            debugCanvas.SetActive(false);
+        }
+    }
+
+    void Start ()
+    {
+        world.SetActive(true);
+        player.SetActive(true);
+        worldCamera.SetActive(true);
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
